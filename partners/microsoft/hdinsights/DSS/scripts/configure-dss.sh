54a2681edb53df95ec0f5d48c5a510d34597767b
Thomas Cabrol <thomas.cabrol@dataiku.com>
2016-09-07 23:25:27 +0200

    Indents

--- partners/microsoft/hdinsights/DSS/scripts/configure-dss.sh
+++ partners/microsoft/hdinsights/DSS/scripts/configure-dss.sh
@@ -8,23 +8,23 @@ export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 
 globalUpdate() {
-	echo "Updating the machine...";	
+	echo "Updating the machine...";
 	sudo su
 	gpg --keyserver pgp.mit.edu --recv-keys B9733A7A07513CAD
 	gpg -a --export 07513CAD | apt-key add -
 	apt-get -y update
-	apt-get -y upgrade	
-    echo "[ERROR] Failed to update the machine globally..."
-    exit 132
+	apt-get -y upgrade
+	echo "[ERROR] Failed to update the machine globally..."
+	exit 132
 }
 
-prepareInstall() {	
+prepareInstall() {
 	echo "Preparing the install...";
-	sudo su	
+	sudo su
 	mkdir -p /mnt/dataiku
 	chown dataiku:dataiku /mnt/dataiku
-    echo "[ERROR] Failed to prepare install..."
-    exit 133
+	echo "[ERROR] Failed to prepare install..."
+	exit 133
 }
 
 installDSS() {
@@ -41,8 +41,8 @@ installDSS() {
 	cd dataiku-dss-3.1.2/
 	sudo -i "/home/dataiku/installers/dataiku/dataiku-dss-3.1.2/scripts/install/install-deps.sh" -yes -without-java -with-r
 	/home/dataiku/installers/dataiku/dataiku-dss-3.1.2/installer.sh -p 20000 -d /mnt/dataiku/dss-data-dir
-    echo "[ERROR] Failed to install DSS..."
-    exit 134
+	echo "[ERROR] Failed to install DSS..."
+	exit 134
 }
 
 configureDSS() {
@@ -55,10 +55,9 @@ configureDSS() {
 	/mnt/dataiku/dss-data-dir/bin/dssadmin install-h2o-integration
 	#/mnt/dataiku/dss-data-dir/bin/dssadmin install-R-integration
 	/mnt/dataiku/dss-data-dir/bin/dss start
-    echo "[ERROR] Failed to configure DSS..."
-    exit 135
+	echo "[ERROR] Failed to configure DSS..."
+	exit 135
 }
-
 
 globalUpdate
 prepareInstall
