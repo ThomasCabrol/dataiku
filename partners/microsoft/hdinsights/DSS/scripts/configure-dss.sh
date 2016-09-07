65e6dc05bda637ffcef1be55aa13c0b56c3717c6
Thomas Cabrol <thomas.cabrol@dataiku.com>
2016-09-07 23:22:56 +0200

    Trying to captur errors

--- partners/microsoft/hdinsights/DSS/scripts/configure-dss.sh
+++ partners/microsoft/hdinsights/DSS/scripts/configure-dss.sh
@@ -1,40 +1,66 @@
 #! /bin/bash
-echo "Configuration of the edge node"
+echo "Configuration of DSS on the edge node"
 
-# Global update
-sudo su
+DSS=3.1.2
 
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 
-gpg --keyserver pgp.mit.edu --recv-keys B9733A7A07513CAD
-gpg -a --export 07513CAD | apt-key add -
-apt-get -y update
-apt-get -y upgrade
+globalUpdate() {
+	echo "Updating the machine...";	
+	sudo su
+	gpg --keyserver pgp.mit.edu --recv-keys B9733A7A07513CAD
+	gpg -a --export 07513CAD | apt-key add -
+	apt-get -y update
+	apt-get -y upgrade	
+    echo "[ERROR] Failed to update the machine globally..."
+    exit 132
+}
 
-# Preparing DSS installation
-mkdir -p /mnt/dataiku
-chown dataiku:dataiku /mnt/dataiku
+prepareInstall() {	
+	echo "Preparing the install...";
+	sudo su	
+	mkdir -p /mnt/dataiku
+	chown dataiku:dataiku /mnt/dataiku
+    echo "[ERROR] Failed to prepare install..."
+    exit 133
+}
 
-# Installing DSS
-su dataiku
-cd /home/dataiku
-mkdir -p /home/dataiku/installers
-mkdir -p /home/dataiku/installers/dataiku
-cd /home/dataiku/installers/dataiku
-wget http://downloads.dataiku.com/public/studio/3.1.2/dataiku-dss-3.1.2.tar.gz
-tar -xzf dataiku-dss-3.1.2.tar.gz
-rm dataiku-dss-3.1.2.tar.gz
-cd dataiku-dss-3.1.2/
-sudo -i "/home/dataiku/installers/dataiku/dataiku-dss-3.1.2/scripts/install/install-deps.sh" -yes -without-java -with-r
-/home/dataiku/installers/dataiku/dataiku-dss-3.1.2/installer.sh -p 20000 -d /mnt/dataiku/dss-data-dir
+installDSS() {
+	echo "Installing DSS...";
+	sudo su
+	su dataiku
+	cd /home/dataiku
+	mkdir -p /home/dataiku/installers
+	mkdir -p /home/dataiku/installers/dataiku
+	cd /home/dataiku/installers/dataiku
+	wget http://downloads.dataiku.com/public/studio/3.1.2/dataiku-dss-3.1.2.tar.gz
+	tar -xzf dataiku-dss-3.1.2.tar.gz
+	rm dataiku-dss-3.1.2.tar.gz
+	cd dataiku-dss-3.1.2/
+	sudo -i "/home/dataiku/installers/dataiku/dataiku-dss-3.1.2/scripts/install/install-deps.sh" -yes -without-java -with-r
+	/home/dataiku/installers/dataiku/dataiku-dss-3.1.2/installer.sh -p 20000 -d /mnt/dataiku/dss-data-dir
+    echo "[ERROR] Failed to install DSS..."
+    exit 134
+}
 
-# Configuring DSS
-cd /mnt/dataiku/dss-data-dir
-/mnt/dataiku/dss-data-dir/bin/dssadmin install-hadoop-integration
-/mnt/dataiku/dss-data-dir/bin/dssadmin install-spark-integration
-/mnt/dataiku/dss-data-dir/bin/dssadmin install-h2o-integration
-#/mnt/dataiku/dss-data-dir/bin/dssadmin install-R-integration
+configureDSS() {
+	echo "Configuring DSS...";
+	sudo su
+	su dataiku
+	cd /mnt/dataiku/dss-data-dir
+	/mnt/dataiku/dss-data-dir/bin/dssadmin install-hadoop-integration
+	/mnt/dataiku/dss-data-dir/bin/dssadmin install-spark-integration
+	/mnt/dataiku/dss-data-dir/bin/dssadmin install-h2o-integration
+	#/mnt/dataiku/dss-data-dir/bin/dssadmin install-R-integration
+	/mnt/dataiku/dss-data-dir/bin/dss start
+    echo "[ERROR] Failed to configure DSS..."
+    exit 135
+}
 
-/mnt/dataiku/dss-data-dir/bin/dss start
+
+globalUpdate
+prepareInstall
+installDSS
+configureDSS
