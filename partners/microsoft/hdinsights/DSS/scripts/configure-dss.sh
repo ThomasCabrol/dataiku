74802e8ec9dede4948a24fc430a7b342bea2dd28
Thomas Cabrol <thomas.cabrol@dataiku.com>
2016-09-08 16:07:20 +0200

    Still figuring out things

--- partners/microsoft/hdinsights/DSS/scripts/configure-dss.sh
+++ partners/microsoft/hdinsights/DSS/scripts/configure-dss.sh
@@ -1,35 +1,35 @@
 #! /bin/bash
-echo "[+] Configuration of DSS on the edge node"
-echo `whoami` > /tmp/whoami
-#DSS=3.1.2
+
+#===================================================#
+# Configuration of DSS on the edge node				#
+#===================================================#
 
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 
-echo "[+] Updating the machine...";
-sudo sh -c 'gpg --keyserver pgp.mit.edu --recv-keys B9733A7A07513CAD'
-sudo sh -c 'gpg -a --export 07513CAD | apt-key add -'
-sudo sh -c 'apt-get -y update'
-sudo sh -c 'apt-get -y upgrade'
+# Global update of the machine
+gpg --keyserver pgp.mit.edu --recv-keys B9733A7A07513CAD
+gpg -a --export 07513CAD | apt-key add -
+apt-get -y update
+apt-get -y upgrade
 
-echo "[+] Preparing the install...";
-sudo sh -c 'mkdir -p /mnt/dataiku'
-sudo sh -c 'chown dataiku:dataiku /mnt/dataiku'
+# Preparing the install 
+mkdir -p /mnt/dataiku
+chown dataiku:dataiku /mnt/dataiku
 
-echo "[+] Installing DSS...";
-sudo su -
-su dataiku
-cd /home/dataiku
+# Downloading DSS
 mkdir -p /home/dataiku/installers
 mkdir -p /home/dataiku/installers/dataiku
-cd /home/dataiku/installers/dataiku
-wget http://downloads.dataiku.com/public/studio/3.1.2/dataiku-dss-3.1.2.tar.gz
-tar -xzf dataiku-dss-3.1.2.tar.gz
-rm dataiku-dss-3.1.2.tar.gz
-cd /home/dataiku/installers/dataiku/dataiku-dss-3.1.2/
-sudo sh -c '"/home/dataiku/installers/dataiku/dataiku-dss-3.1.2/scripts/install/install-deps.sh" -yes -without-java -with-r'
-/home/dataiku/installers/dataiku/dataiku-dss-3.1.2/installer.sh -p 20000 -d /mnt/dataiku/dss-data-dir
+chown -R dataiku:dataiku /home/dataiku
+sudo -u dataiku sh -c "wget http://downloads.dataiku.com/public/studio/3.1.2/dataiku-dss-3.1.2.tar.gz -P /home/dataiku/installers/dataiku/"
+
+#
+#tar -xzf dataiku-dss-3.1.2.tar.gz
+#rm dataiku-dss-3.1.2.tar.gz
+#cd /home/dataiku/installers/dataiku/dataiku-dss-3.1.2/
+#sudo sh -c '"/home/dataiku/installers/dataiku/dataiku-dss-3.1.2/scripts/install/install-deps.sh" -yes -without-java -with-r'
+#/home/dataiku/installers/dataiku/dataiku-dss-3.1.2/installer.sh -p 20000 -d /mnt/dataiku/dss-data-dir
 
 #echo "[+] Configuring DSS...";
 #cd /mnt/dataiku/dss-data-dir
