1f4e1cca48d21a8dbdcd51425ebd86c36c24348a
Thomas Cabrol <thomas.cabrol@dataiku.com>
2016-09-08 23:12:49 +0200

    Add custom shell script

--- partners/microsoft/hdinsights/dataiku-dss/scripts/configure-dss.sh
+++ partners/microsoft/hdinsights/dataiku-dss/scripts/configure-dss.sh
@@ -13,3 +13,22 @@ gpg --keyserver pgp.mit.edu --recv-keys B9733A7A07513CAD
 gpg -a --export 07513CAD | apt-key add -
 apt-get -y update
 apt-get -y upgrade
+
+# Preparing the install 
+mkdir -p /mnt/dataiku
+chown dataiku:dataiku /mnt/dataiku
+
+# Downloading DSS
+mkdir -p /home/dataiku/installers
+mkdir -p /home/dataiku/installers/dataiku
+chown -R dataiku:dataiku /home/dataiku
+sudo -u dataiku sh -c "wget http://downloads.dataiku.com/public/studio/3.1.2/dataiku-dss-3.1.2.tar.gz -P /home/dataiku/installers/dataiku/"
+sudo -u dataiku sh -c "cd /home/dataiku/installers/dataiku/ ; tar -xzf dataiku-dss-3.1.2.tar.gz ; rm dataiku-dss-3.1.2.tar.gz"
+sudo -i "/home/dataiku/installers/dataiku/dataiku-dss-3.1.2/scripts/install/install-deps.sh" -yes -without-java -with-r
+sudo -u dataiku sh -c "/home/dataiku/installers/dataiku/dataiku-dss-3.1.2/installer.sh -p 20000 -d /mnt/dataiku/dss-data-dir"
+
+# Configuring & starting DSS
+sudo -u dataiku sh -c "/mnt/dataiku/dss-data-dir/bin/dssadmin install-hadoop-integration"
+sudo -u dataiku sh -c "/mnt/dataiku/dss-data-dir/bin/dssadmin install-spark-integration"
+sudo -u dataiku sh -c "/mnt/dataiku/dss-data-dir/bin/dssadmin install-h2o-integration"
+sudo -u dataiku sh -c "/mnt/dataiku/dss-data-dir/bin/dss start"
